357ace9721a8bac995c4649e951c702fa82a65b7
MaheshKamble <sahara@DESKTOP-B2Q58QP>
2022-11-17 18:24:29 +0530

    Code Added by Mahesh

--- GitPractice/Program.cs
+++ GitPractice/Program.cs
@@ -7,7 +7,6 @@ namespace GitPractice
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello Swapnil & Tushar");
 
         }
     }
